dd565cff44c8695172bd41d8cf67889337d0acb6
Hyerin Jo <21500701@handong.edu>
2018-04-12 21:20:24 +0900

    commit2

---
+++ src/main/java/edu/handong/csee/java/lab09/prob1/SalesAssociate.java
@@ -0,0 +1,29 @@
+package edu.handong.csee.java.lab09.prob1;
+
+public class SalesAssociate {
+	
+	private String name;
+	private double sales;
+	
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setSales(double sales)
+	{
+		this.sales = sales;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public double getSales()
+	{
+		return sales;
+	}
+
+}
