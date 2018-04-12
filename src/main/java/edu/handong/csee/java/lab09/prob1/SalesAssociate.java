c210439576639400d0e0474e40288a6e6e84ae3c
Hyerin Jo <21500701@handong.edu>
2018-04-12 21:44:04 +0900

    commit3

--- src/main/java/edu/handong/csee/java/lab09/prob1/SalesAssociate.java
+++ src/main/java/edu/handong/csee/java/lab09/prob1/SalesAssociate.java
@@ -1,26 +1,39 @@
 package edu.handong.csee.java.lab09.prob1;
-
+/**
+ * a class that contains an array and methods that get data and put data into an array
+ */
 public class SalesAssociate {
 	
 	private String name;
 	private double sales;
 	
-	
+	/**
+	 * a method 
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * a method 
+	 */
 	public void setSales(double sales)
 	{
 		this.sales = sales;
 	}
 	
+	/**
+	 * a method 
+	 */
 	public String getName()
 	{
 		return name;
 	}
 	
+	/**
+	 * a method 
+	 */
 	public double getSales()
 	{
 		return sales;
